ab695a5a6a3c8299f24b7a1dd316dfce57a4277f
KrTv <krtviv@mail.ru>
2026-02-02 19:49:45 +0300

    6.5. Динамические массивы

--- main.go
+++ main.go
@@ -3,15 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	transactions := [6]int{1, 2, 3, 4, 5, 6}
-	transactionsPartial := transactions[1:5]
-	transactionsNewPartial := transactionsPartial[:1]
-	transactionsNewPartial[0] = 30
-
-	transactionsNewPartial = transactionsNewPartial[0:4]
-
+	transactions := []int{0, 20, 35}
+	temp := transactions
+	transactions = append(transactions, 100)
 	fmt.Println(transactions)
-	fmt.Println(transactionsNewPartial)
-	fmt.Println(len(transactionsPartial), cap(transactionsPartial))
-	fmt.Println(len(transactionsNewPartial), cap(transactionsNewPartial))
+	fmt.Println(temp)
 }
