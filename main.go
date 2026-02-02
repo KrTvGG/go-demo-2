3404159a107a2c0df9e936fc26d52bd8a20a06c4
KrTv <krtviv@mail.ru>
2026-02-02 19:40:37 +0300

    6.4. Cap и len

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	transactions := [5]int{1, 2, 3, 4, 5}
-	banks := [2]string{}
+	transactions := [6]int{1, 2, 3, 4, 5, 6}
+	transactionsPartial := transactions[1:5]
+	transactionsNewPartial := transactionsPartial[:1]
+	transactionsNewPartial[0] = 30
 
-	fmt.Println(transactions[1])
-	banks[0] = "Тинкофф"
-	fmt.Println(banks)
-	partial := transactions[1:4]
-	fmt.Println(partial)
+	transactionsNewPartial = transactionsNewPartial[0:4]
+
+	fmt.Println(transactions)
+	fmt.Println(transactionsNewPartial)
+	fmt.Println(len(transactionsPartial), cap(transactionsPartial))
+	fmt.Println(len(transactionsNewPartial), cap(transactionsNewPartial))
 }
