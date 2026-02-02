ffeb4c3559dfd63b0b929f00e3563f0718cc6b26
KrTv <krtviv@mail.ru>
2026-02-02 19:26:16 +0300

    6.3. Slice

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	transactions := [5]int{1, 2, 3, 4, 5}
+	banks := [2]string{}
+
+	fmt.Println(transactions[1])
+	banks[0] = "Тинкофф"
+	fmt.Println(banks)
+	partial := transactions[1:4]
+	fmt.Println(partial)
 }
